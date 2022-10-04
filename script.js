fe3c5afb879d72278ce82498a639bec231873870
Yuan Zhang <yu_an12@163.com>
2022-10-04 18:22:45 -0400

    updates

--- script.js
+++ script.js
@@ -35,14 +35,69 @@ window.onload = function() {
         // Use the arcTo command to draw cloudy lines
         path.arcTo(event.point);
     }
-    let svg = `text.svg`;
 
-    paper.project.importSVG(svg, function(item) {
-        daydream = item
-        daydream.position = new Point(daydream.bounds.width / 2, daydream.bounds.height / 2)
-    })
 
     function onResize(event) {
         initializePath();
     }
+}
+
+
+
+
+
+var width, height, center;
+var points = 10;
+var smooth = true;
+var path = new Path();
+var mousePos = view.center / 2;
+var pathHeight = mousePos.y;
+path.fillColor = 'black';
+initializePath();
+
+function initializePath() {
+    center = view.center;
+    width = view.size.width;
+    height = view.size.height / 2;
+    path.segments = [];
+    path.add(view.bounds.bottomLeft);
+    for (var i = 1; i < points; i++) {
+        var point = new Point(width / points * i, center.y);
+        path.add(point);
+    }
+    path.add(view.bounds.bottomRight);
+    path.fullySelected = true;
+}
+
+function onFrame(event) {
+    pathHeight += (center.y - mousePos.y - pathHeight) / 10;
+    for (var i = 1; i < points; i++) {
+        var sinSeed = event.count + (i + i % 10) * 100;
+        var sinHeight = Math.sin(sinSeed / 200) * pathHeight;
+        var yPos = Math.sin(sinSeed / 100) * sinHeight + height;
+        path.segments[i].point.y = yPos;
+    }
+    if (smooth)
+        path.smooth({ type: 'continuous' });
+}
+
+function onMouseMove(event) {
+    mousePos = event.point;
+}
+
+function onMouseDown(event) {
+    smooth = !smooth;
+    if (!smooth) {
+        // If smooth has been turned off, we need to reset
+        // the handles of the path:
+        for (var i = 0, l = path.segments.length; i < l; i++) {
+            var segment = path.segments[i];
+            segment.handleIn = segment.handleOut = null;
+        }
+    }
+}
+
+// Reposition the path whenever the window is resized:
+function onResize(event) {
+    initializePath();
 }
